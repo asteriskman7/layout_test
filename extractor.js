2860fc2542209d5a4f0050d6fe216637d7c4a759
asteriskman <asteriskman@gmail.com>
2015-11-13 11:17:43 -0500

    getting initial layout canvas drawing to work

--- extractor.js
+++ extractor.js
@@ -1,5 +1,123 @@
 'use strict';
 
 var extractor = {
+  extract: function(layoutData) {
+    
+    
+// vias only conduct in the Z direction
+// pc only vertical
+// m1/m2 any direction
+// pfets/nfets must have minimum width. must have length of 1
+// 
+// make derived layers:
+// pc and pwell => pfet
+// pc and nwel => nfet
+// pwell and not pc => pw
+// nwell and not pc => nw
+// 
+// all active adjacent cells on a layer are connected, adjacent is nsew, not diagonal
+// 
+// foreach label in the design
+//   propagate the net via floodfill
+// 
+// foreach active cell in all layers
+//   if it's not already in a net
+//     assign it a new net
+//     propagate the net via floodfill
+//     //floodfill can't find another net because if it does then the other net would have
+//     // already floodfilled this cell
+// 
+// foreach pfet and nfet
+//   create device with appropriate connections
+
+    var pfets = extractor.deriveLayer('and', layoutData.layers.pc, layoutData.layers.pwell);
+    var nfets = extractor.deriveLayer('and', layoutData.layers.pc, layoutData.layers.nwell);
+    
+    return {};
+    
+  },
   
+  deriveLayer: function(op, a, b) {
+    if (a.length != b.length) {
+      throw 'both layers in deriveLayer must be the same length';
+    }
+    
+    var z = [];
+    var aval;
+    var bval;
+    
+    for (var i = 0; i < a.length; i++) {
+      aval = a[i];
+      bval = b[i];
+      switch (op) {
+        case 'not':
+          z.push(1-aval);
+          break;
+        case 'and':
+          z.push(aval & bval);
+          break;
+        case 'or':
+          z.push(zval | bval);
+      }
+    }
+    
+    return z;
+  },
+  
+  joinCells: function(layer, w, h, x, y, checkedCells) {
+    var cellGroup = [];
+    var cellsToCheck = [];
+    //var checkedCells = [];
+    if (checkedCells === undefined) {
+      checkedCells = [];
+    }
+    var curCell;
+    var i;
+    i = y * w + x;
+    if ((layer[i] === 1) && (checkedCells[i] !== 1)) {
+      cellGroup.push(i);
+      cellsToCheck.push({x: x, y: y-1});
+      cellsToCheck.push({x: x, y: y+1});
+      cellsToCheck.push({x: x-1, y: y});
+      cellsToCheck.push({x: x+1, y: y});
+      checkedCells[y*w + x] = 1;
+
+      while (cellsToCheck.length > 0) {
+        curCell = cellsToCheck.pop();
+        x = curCell.x;
+        y = curCell.y
+        i = y * w + x;
+        if ((layer[i] === 1) && (checkedCells[i] !== 1)) {
+          cellGroup.push(i);
+          cellsToCheck.push({x: x, y: y-1});
+          cellsToCheck.push({x: x, y: y+1});
+          cellsToCheck.push({x: x-1, y: y});
+          cellsToCheck.push({x: x+1, y: y});
+        }
+        checkedCells[i] = 1;
+      }
+    }
+    
+    return {cellGroup: cellGroup, checkedCells: checkedCells};
+  },
+  
+  extractLayerGroups: function(layer, w, h) {
+    var layerGroups = [];
+    var checkedCells = [];
+    var joinResult;
+    var i;
+    for (var x = 0; x < w; x++) {
+      for (var y = 0; y < h; y++) {
+        i = y * w + x;
+        if (checkedCells[i] !== 1) {
+          joinResult = extractor.joinCells(layer, w, h, x, y, checkedCells);
+          checkedCells = joinResult.checkedCells;
+          if (joinResult.cellGroup.length > 0) {
+            layerGroups.push(joinResult.cellGroup);
+          }
+        }
+      }
+    }
+    return layerGroups;
+  }
 };
--- main.js
+++ main.js
@@ -1,3 +1,111 @@
 'use strict';
 
 console.log('init');
+
+function getImageData() {
+  return {
+    width: 9,
+    height: 19,
+    layers: {
+      nwell: [0,0,0,0,0,0,0,0,0, 0,0,0,0,0,0,0,0,0, 0,0,0,0,0,0,0,0,0, 0,0,0,0,0,0,0,0,0, 0,0,0,0,0,0,0,0,0, 0,0,0,0,0,0,0,0,0, 0,0,0,0,0,0,0,0,0, 0,0,0,0,0,0,0,0,0, 0,0,0,0,0,0,0,0,0, 0,0,0,0,0,0,0,0,0, 0,0,0,0,0,0,0,0,0, 0,0,0,0,0,0,0,0,0, 1,1,1,1,1,1,1,1,1, 1,1,1,1,1,1,1,1,1, 1,1,1,1,1,1,1,1,1, 1,1,1,1,1,1,1,1,1, 0,0,0,0,0,0,0,0,0, 0,0,0,0,0,0,0,0,0, 0,0,0,0,0,0,0,0,0],
+      pwell: [0,0,0,0,0,0,0,0,0, 0,0,0,0,0,0,0,0,0, 0,0,0,0,0,0,0,0,0, 1,1,1,1,1,1,1,1,1, 1,1,1,1,1,1,1,1,1, 1,1,1,1,1,1,1,1,1, 1,1,1,1,1,1,1,1,1, 0,0,0,0,0,0,0,0,0, 0,0,0,0,0,0,0,0,0, 0,0,0,0,0,0,0,0,0, 0,0,0,0,0,0,0,0,0, 0,0,0,0,0,0,0,0,0, 0,0,0,0,0,0,0,0,0, 0,0,0,0,0,0,0,0,0, 0,0,0,0,0,0,0,0,0, 0,0,0,0,0,0,0,0,0, 0,0,0,0,0,0,0,0,0, 0,0,0,0,0,0,0,0,0, 0,0,0,0,0,0,0,0,0],
+      pc:    [0,0,0,0,0,0,0,0,0, 0,0,0,0,0,0,0,0,0, 0,0,0,0,1,0,0,0,0, 0,0,0,0,1,0,0,0,0, 0,0,0,0,1,0,0,0,0, 0,0,0,0,1,0,0,0,0, 0,0,0,0,1,0,0,0,0, 0,0,0,0,1,0,0,0,0, 0,0,0,0,1,0,0,0,0, 0,0,0,0,1,0,0,0,0, 0,0,0,0,1,0,0,0,0, 0,0,0,0,1,0,0,0,0, 0,0,0,0,1,0,0,0,0, 0,0,0,0,1,0,0,0,0, 0,0,0,0,1,0,0,0,0, 0,0,0,0,1,0,0,0,0, 0,0,0,0,1,0,0,0,0, 0,0,0,0,0,0,0,0,0, 0,0,0,0,0,0,0,0,0],
+      v0:    [0,0,0,0,0,0,0,0,0, 0,0,0,0,0,0,0,0,0, 0,0,0,0,0,0,0,0,0, 0,0,0,0,0,0,0,0,0, 0,0,1,0,0,0,0,0,0, 0,0,0,0,0,0,0,1,0, 0,0,0,0,0,0,0,0,0, 0,0,0,0,0,0,0,0,0, 0,0,0,0,0,0,0,0,0, 0,0,0,0,1,0,0,0,0, 0,0,0,0,0,0,0,0,0, 0,0,0,0,0,0,0,0,0, 0,0,0,0,0,0,0,0,0, 0,0,0,0,0,0,0,1,0, 0,0,1,0,0,0,0,0,0, 0,0,0,0,0,0,0,0,0, 0,0,0,0,0,0,0,0,0, 0,0,0,0,0,0,0,0,0, 0,0,0,0,0,0,0,0,0],
+      m1:    [1,1,1,1,1,1,1,1,1, 0,0,1,0,0,0,0,0,0, 0,0,1,0,0,0,0,0,0, 0,0,1,0,0,0,0,0,0, 0,0,1,0,0,0,0,0,0, 0,0,0,0,0,0,0,1,0, 0,0,0,0,0,0,0,1,0, 0,0,0,0,0,0,0,1,0, 0,0,0,0,0,0,0,1,0, 1,1,1,1,1,0,0,1,1, 0,0,0,0,0,0,0,1,0, 0,0,0,0,0,0,0,1,0, 0,0,0,0,0,0,0,1,0, 0,0,0,0,0,0,0,1,0, 0,0,1,0,0,0,0,0,0, 0,0,1,0,0,0,0,0,0, 0,0,1,0,0,0,0,0,0, 0,0,1,0,0,0,0,0,0, 1,1,1,1,1,1,1,1,1],
+      v1:    [1,0,1,0,1,0,1,0,1, 0,0,0,0,0,0,0,0,0, 0,0,0,0,0,0,0,0,0, 0,0,0,0,0,0,0,0,0, 0,0,0,0,0,0,0,0,0, 0,0,0,0,0,0,0,0,0, 0,0,0,0,0,0,0,0,0, 0,0,0,0,0,0,0,0,0, 0,0,0,0,0,0,0,0,0, 0,0,0,0,0,0,0,0,0, 0,0,0,0,0,0,0,0,0, 0,0,0,0,0,0,0,0,0, 0,0,0,0,0,0,0,0,0, 0,0,0,0,0,0,0,0,0, 0,0,0,0,0,0,0,0,0, 0,0,0,0,0,0,0,0,0, 0,0,0,0,0,0,0,0,0, 0,0,0,0,0,0,0,0,0, 0,0,0,0,0,0,0,0,0],
+      m2:    [1,1,1,1,1,1,1,1,1, 1,1,1,1,1,1,1,1,1, 0,0,0,0,0,0,0,0,0, 0,0,0,0,0,0,0,0,0, 0,0,0,0,0,0,0,0,0, 0,0,0,0,0,0,0,0,0, 0,0,0,0,0,0,0,0,0, 0,0,0,0,0,0,0,0,0, 0,0,0,0,0,0,0,0,0, 0,0,0,0,0,0,0,0,0, 0,0,0,0,0,0,0,0,0, 0,0,0,0,0,0,0,0,0, 0,0,0,0,0,0,0,0,0, 0,0,0,0,0,0,0,0,0, 0,0,0,0,0,0,0,0,0, 0,0,0,0,0,0,0,0,0, 0,0,0,0,0,0,0,0,0, 0,0,0,0,0,0,0,0,0, 0,0,0,0,0,0,0,0,0]
+    }
+  };
+}
+
+
+
+function printLayer(w, h, data) {
+  var row;
+  for(var y = 0; y < h; y++) {
+    row = '';
+    for (var x = 0; x < w; x++) {
+      //console.log(x + ' ' + y);
+      row += data[y * w + x].toString();
+    }
+    //console.log(y + ' ' + row);
+    console.log(row); 
+  }
+}
+
+var layoutData = getImageData();
+
+var design = extractor.extract(layoutData);
+
+function drawLayout(ctx, canvasW, canvasH, layoutData) {
+  var gridSize = 15;
+  var gridOriginX = 0;
+  var gridOriginY = 0;
+  console.log('drawing layout');
+  ctx.clearRect(0, 0, canvasW, canvasH);
+  ctx.save();
+  ctx.translate(-gridSize *  gridOriginX, -gridSize * gridOriginY);
+  //draw layers
+  var layerList = [
+    {name: 'pwell', type: 'normal', color: 'rgba(0,255,0,1)'},
+    {name: 'nwell', type: 'normal', color: 'rgba(255,255,0,1)'},
+    {name: 'pc', type: 'normal', color: 'rgba(255, 0, 255, 0.75)'},
+    {name: 'm1', type: 'normal', color: 'rgba(0, 0, 255, 0.75)'},
+    {name: 'v0', type: 'via', color: 'rgba(255, 255, 255, 1)'},
+    {name: 'm2', type: 'normal', color: 'rgba(255, 0, 0, 0.75)'},
+    {name: 'v1', type: 'via', color: 'rgba(255, 255, 0, 1)'},
+  ];
+  var layerData;  
+  
+  layerList.forEach(
+    function(v) {
+       layerData = layoutData.layers[v.name];
+       if (layerData !== undefined) {        
+        for (var y = 0; y < layoutData.height; y++) {
+          for (var x = 0; x < layoutData.width; x++) { 
+            if (layerData[y * layoutData.width + x] === 1) {
+              switch (v.type) {
+                case 'normal': 
+                  ctx.fillStyle = v.color;
+                  ctx.fillRect(x * gridSize, y * gridSize, gridSize, gridSize);
+                  break;
+                case 'via':
+                  ctx.strokeStyle = v.color;
+                  ctx.beginPath();
+                  ctx.arc(x * gridSize + gridSize * 0.5 + 0.5, y * gridSize + gridSize * 0.5 + 0.5, gridSize * 0.5 * 0.75, 0, 2 * Math.PI);
+                  ctx.stroke();
+                  break;
+              }
+              
+            }
+          }
+        }
+       }
+       
+    }
+  );
+  
+  ctx.restore();
+  ctx.save();
+  //draw grid
+  ctx.strokeStyle = '#000000';
+  ctx.lineWidth = 1;
+  for (var x = 1; x < Math.ceil(canvasW / gridSize); x++) {
+    ctx.beginPath();
+    ctx.moveTo(x * gridSize+0.5, 0);
+    ctx.lineTo(x * gridSize+0.5, canvasH);
+    ctx.stroke();
+  }
+  for (var y = 1; y < Math.ceil(canvasH / gridSize); y++) {
+    ctx.beginPath();
+    ctx.moveTo(0,       y * gridSize+0.5);
+    ctx.lineTo(canvasW, y * gridSize+0.5);
+    ctx.stroke();
+  }
+  ctx.restore();
+}
+
+var canvas = document.getElementById('canvas_layout');
+var ctx = canvas.getContext('2d');
+
+drawLayout(ctx, canvas.width, canvas.height, layoutData);
