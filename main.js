cf86e99125b5255975eb3294714766a7607e75e4
asteriskman <asteriskman@gmail.com>
2015-11-13 22:39:10 -0500

    remove old printLayer function add resizeLayer function add resizeAllLayers function update drawLayout to expect layout data to match canvas size add ability to display selection of cells add function for new redraw button

--- main.js
+++ main.js
@@ -18,29 +18,39 @@ function getImageData() {
   };
 }
 
-
-
-function printLayer(w, h, data) {
-  var row;
-  for(var y = 0; y < h; y++) {
-    row = '';
-    for (var x = 0; x < w; x++) {
-      //console.log(x + ' ' + y);
-      row += data[y * w + x].toString();
+function resizeLayer(layer, oldW, oldH, newW, newH) {
+  var result = [];
+  var newVal;
+  for (var y = 0; y < newH; y++) {
+    for (var x = 0; x < newW; x++) {
+      if ((y >= oldH) || (x >= oldW)) {
+        newVal = 0;
+      } else {
+        newVal = layer[y * oldW + x];
+      }
+      result[y * newW + x] = newVal;
     }
-    //console.log(y + ' ' + row);
-    console.log(row); 
   }
+  return result;
 }
 
-var layoutData = getImageData();
+function resizeAllLayers(layoutData, newW, newH) {
+  var oldW = layoutData.width;
+  var oldH = layoutData.height;
+  layoutData.width = newW;
+  layoutData.height = newH;
+  for (var layerName in layoutData.layers) {
+    layoutData.layers[layerName] = resizeLayer(layoutData.layers[layerName], oldW, oldH, newW, newH);
+  }
+  return layoutData;
+}
 
-var design = extractor.extract(layoutData);
-
-function drawLayout(ctx, canvasW, canvasH, layoutData) {
+function drawLayout(ctx, canvasW, canvasH, layoutData, selection) {
   var gridSize = 15;
   var gridOriginX = 0;
   var gridOriginY = 0;
+  var gridWidth = Math.floor(canvasW / gridSize);
+  var gridHeight = Math.floor(canvasH / gridSize);
   console.log('drawing layout');
   ctx.clearRect(0, 0, canvasW, canvasH);
   ctx.save();
@@ -59,11 +69,11 @@ function drawLayout(ctx, canvasW, canvasH, layoutData) {
   
   layerList.forEach(
     function(v) {
-       layerData = layoutData.layers[v.name];
-       if (layerData !== undefined) {        
-        for (var y = 0; y < layoutData.height; y++) {
-          for (var x = 0; x < layoutData.width; x++) { 
-            if (layerData[y * layoutData.width + x] === 1) {
+      layerData = layoutData.layers[v.name];
+      if (layerData !== undefined) {        
+        for (var y = 0; y < gridHeight; y++) {
+          for (var x = 0; x < gridWidth; x++) { 
+            if (layerData[y * gridWidth + x] === 1) {
               switch (v.type) {
                 case 'normal': 
                   ctx.fillStyle = v.color;
@@ -75,28 +85,40 @@ function drawLayout(ctx, canvasW, canvasH, layoutData) {
                   ctx.arc(x * gridSize + gridSize * 0.5 + 0.5, y * gridSize + gridSize * 0.5 + 0.5, gridSize * 0.5 * 0.75, 0, 2 * Math.PI);
                   ctx.stroke();
                   break;
-              }
-              
+              }              
             }
           }
         }
-       }
-       
+      }       
     }
   );
+  
+  var selectedIndex;
+  var x;
+  var y;
+  for (var i = 0; i < selection.length; i++) {
+    selectedIndex = selection[i];
+    y = Math.floor(selectedIndex / gridWidth);
+    x = selectedIndex % gridWidth;
+    ctx.strokeStyle = 'rgba(255,255,255,1)';
+    ctx.beginPath();
+    ctx.moveTo(x * gridSize, y * gridSize);
+    ctx.lineTo(x * gridSize + gridSize, y * gridSize + gridSize);
+    ctx.stroke();
+  }
   
   ctx.restore();
   ctx.save();
   //draw grid
   ctx.strokeStyle = '#000000';
   ctx.lineWidth = 1;
-  for (var x = 1; x < Math.ceil(canvasW / gridSize); x++) {
+  for (x = 1; x < Math.ceil(canvasW / gridSize); x++) {
     ctx.beginPath();
     ctx.moveTo(x * gridSize+0.5, 0);
     ctx.lineTo(x * gridSize+0.5, canvasH);
     ctx.stroke();
   }
-  for (var y = 1; y < Math.ceil(canvasH / gridSize); y++) {
+  for (y = 1; y < Math.ceil(canvasH / gridSize); y++) {
     ctx.beginPath();
     ctx.moveTo(0,       y * gridSize+0.5);
     ctx.lineTo(canvasW, y * gridSize+0.5);
@@ -105,7 +127,17 @@ function drawLayout(ctx, canvasW, canvasH, layoutData) {
   ctx.restore();
 }
 
+
+
 var canvas = document.getElementById('canvas_layout');
 var ctx = canvas.getContext('2d');
 
-drawLayout(ctx, canvas.width, canvas.height, layoutData);
+var layoutData = resizeAllLayers(getImageData(), 20, 20);
+
+var design = extractor.extract(layoutData);
+
+var selection = extractor.extractLayerGroups(layoutData.layers.m1, 20, 20);
+
+drawLayout(ctx, canvas.width, canvas.height, layoutData, []);
+
+document.getElementById('button_redraw').onclick = function() {drawLayout(ctx, canvas.width, canvas.height, layoutData, [])};
